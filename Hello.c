26dfdd15322f2b5f87bcfcaacd1fa0bae2d6d51b
Sundaram04 <145194021+Sundaram04@users.noreply.github.com>
2023-09-17 00:30:13 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//area of square 
+int main() {
+    int side ;
+    printf("side of square : ");
+    scanf("%d",&side);
+    
+    printf("area of square :%d",side * side);
+
+
+    return 0;
+}
