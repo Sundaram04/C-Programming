8ca6d50a1d2d54c427f0ebe6506605a9f212540a
Sundaram04 <145194021+Sundaram04@users.noreply.github.com>
2023-09-18 23:34:40 +0530

    Add files via upload

---
+++ Program5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// Cube of a number 
+
+int main() {
+    int n ;
+    printf("Enter the number :");
+    scanf("%d",&n);
+
+    printf("Cube of the number : %d",n*n*n);
+
+    return 0;
+}
